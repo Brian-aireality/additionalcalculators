6114dd8ff84b06e6e55ed36c2356ff7d678fd84c
Student <example@example.com>
2019-10-21 11:24:03 -0700

    set up file structure and add bmi calculator to bmi script

---
+++ bmi/scripts.js
@@ -0,0 +1,6 @@
+var bmi = function(weight, height) {
+return weight/height
+}
+var weight = parseFloat(prompt("Enter weight in kilograms"))
+var height = parseFloat(prompt("Enter height in meters squared"))
+alert (bmi(weight, height));
